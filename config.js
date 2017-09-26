079a29bab50fa70eb7c6120659224b8aa31bcc55
Jakob Niemeyer <dev.yoku@gmail.com>
2017-09-26 14:33:59 +0200

    basic config file

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'wine-db',
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3000,
+    db: {
+        uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/api',
+    },
+};
