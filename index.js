ef1ec0204d2125b9c29ae6f3b054af57a3954685
Jakob Niemeyer <dev.yoku@gmail.com>
2017-09-26 16:38:41 +0200

    #1 use eslint for code inspection

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
-'use strict'
+'use strict';
 
-/**
- * Module Dependencies
- */
 const config = require('./config');
 const restify = require('restify');
 const mongodb = require('mongodb').MongoClient;
 
-/**
-  * Initialize Server
-  */
 const server = restify.createServer({
-  name    : config.name,
-	version : config.version,
+  name: config.name,
+  version: config.version,
 });
 
-server.use(restify.plugins.jsonBodyParser({ mapParams: true }));
+server.use(restify.plugins.jsonBodyParser({mapParams: true}));
 server.use(restify.plugins.acceptParser(server.acceptable));
-server.use(restify.plugins.queryParser({ mapParams: true }));
+server.use(restify.plugins.queryParser({mapParams: true}));
 server.use(restify.plugins.fullResponse());
 
-/**
-  * Start Server, Connect to DB & Require Routes
-  */
 server.listen(config.port, () => {
-	// establish connection to mongodb
   mongodb.connect(config.db.uri, (error, db) => {
-
           if (error) {
-              console.error('An error occurred while attempting to connect to MongoDB', error)
+              console.error('An error occurred while '+
+                'attempting to connect to MongoDB', error);
               process.exit(1);
           }
 
-          //require('./routes')({ db, server });
+          // require('./routes')({ db, server });
 
           console.log(
-              '%s v%s ready to accept connections on port %s in %s environment.',
+              '%s v%s ready to accept connections'+
+              'on port %s in %s environment.',
               server.name,
               config.version,
               config.port,
               config.env
           );
   });
-
 });
